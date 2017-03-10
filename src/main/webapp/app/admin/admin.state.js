5183373b6b3ee1f8ea398675ffc238e9c8fe18d8
ming <xiemingzhi@gmail.com>
2017-03-11 06:52:25 +0800

    Basic jhipster app with websockets enabled
    To view goto http://localhost:8080 login admin Administration User Tracker open second browser login user

---
+++ src/main/webapp/app/admin/admin.state.js
@@ -0,0 +1,16 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('jhipsterappApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig ($stateProvider) {
+        $stateProvider.state('admin', {
+            abstract: true,
+            parent: 'app'
+        });
+    }
+})();
---
+++ src/main/webapp/app/services/auth/password.service.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('jhipsterappApp')
+        .factory('Password', Password);
+
+    Password.$inject = ['$resource'];
+
+    function Password($resource) {
+        var service = $resource('api/account/change_password', {}, {});
+
+        return service;
+    }
+})();
